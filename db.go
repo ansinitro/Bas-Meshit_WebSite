ba8b92606c8d228f7f3cc833db8c9867219e6eed
Qydnama <amangikbaktibaiuli@gmail.com>
2024-02-29 22:01:27 +0600

    adding heroku postgres

--- db.go
+++ db.go
@@ -3,6 +3,8 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
+
 	_ "github.com/lib/pq"
 )
 
@@ -10,18 +12,14 @@ var DB *sql.DB
 
 func OpenDB() error {
 	var err error
-	const (
-		host     = "localhost"
-		port     = 7777
-		user     = "postgres"
-		password = "15691804"
-		dbname   = "BasMeshit"
-	)
 
-	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	// Использование DATABASE_URL из переменных окружения
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl == "" {
+		return fmt.Errorf("DATABASE_URL is not set")
+	}
 
-	DB, err = sql.Open("postgres", connStr)
-
+	DB, err = sql.Open("postgres", databaseUrl)
 	if err != nil {
 		return err
 	}
